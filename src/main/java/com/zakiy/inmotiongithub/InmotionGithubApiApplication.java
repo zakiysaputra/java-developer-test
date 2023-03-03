90f48fdb40f3d6598611db2e706043fc697a9ba5
Zakiy Saputra <mzakiysaputra@gmail.com>
2023-03-03 20:20:19 +0700

    First Commit

---
+++ src/main/java/com/zakiy/inmotiongithub/InmotionGithubApiApplication.java
@@ -0,0 +1,13 @@
+package com.zakiy.inmotiongithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmotionGithubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InmotionGithubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zakiy/inmotiongithub/InmotionGithubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zakiy.inmotiongithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InmotionGithubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
